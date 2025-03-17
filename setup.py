0241b4f6d4190feeb1bfe19026c75b3256c366a5
ayonghan <076bei007.ashma@pcampus.edu.np>
2025-03-18 02:10:31 +0545

    feat: model built dvc pipeline

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="sentiment-analyzer",
+    version="0.0.0",
+    author="Ashma",
+    author_email="yonghangaashma@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
